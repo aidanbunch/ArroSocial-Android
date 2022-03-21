ab5b808c70e4b59b84b22e96d367b282dfd19382
aidanbunch <aidanb08@icloud.com>
2022-03-21 11:22:17 -0700

    Added splash screen animation

--- app/src/main/java/com/aidanbunch/arrosocial/SplashActivity.java
+++ app/src/main/java/com/aidanbunch/arrosocial/SplashActivity.java
@@ -1,8 +1,16 @@
 package com.aidanbunch.arrosocial;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.MotionLayout;
+import androidx.constraintlayout.motion.widget.TransitionAdapter;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +18,78 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        hideToolBar();
+
+        MotionLayout splashLayout = (MotionLayout) findViewById(R.id.splashLayout);
+
+        splashLayout.setTransitionListener(new TransitionAdapter() {
+            public void onTransitionChange(MotionLayout motionLayout, int startId, int endId, float progress) {
+            }
+            @Override
+            public void onTransitionCompleted(MotionLayout motionLayout, int currentId) {
+
+                int colorFrom = getResources().getColor(R.color.purple);
+                int colorTo = getResources().getColor(R.color.off_white);
+                ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
+                colorAnimation.setDuration(400);
+                colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                        motionLayout.setBackgroundColor((int) valueAnimator.getAnimatedValue());
+                    }
+                });
+                colorAnimation.start();
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                finish();
+            }
+            public void onTransitionStarted(MotionLayout motionLayout, int startId, int endId) {
+            }
+            public void onTransitionTrigger(MotionLayout motionLayout, int triggerId, boolean positive, float progress) {
+            }
+        });
+    }
+
+    public void hideToolBar(){
+
+        // BEGIN_INCLUDE (get_current_ui_flags)
+        // The UI options currently enabled are represented by a bitfield.
+        // getSystemUiVisibility() gives us that bitfield.
+        int uiOptions = getWindow().getDecorView().getSystemUiVisibility();
+        int newUiOptions = uiOptions;
+        // END_INCLUDE (get_current_ui_flags)
+        // BEGIN_INCLUDE (toggle_ui_flags)
+        boolean isImmersiveModeEnabled =
+                ((uiOptions | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) == uiOptions);
+
+        // Navigation bar hiding:  Backwards compatible to ICS.
+        if (Build.VERSION.SDK_INT >= 14) {
+            newUiOptions ^= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        }
+
+        // Status bar hiding: Backwards compatible to Jellybean
+        if (Build.VERSION.SDK_INT >= 16) {
+            newUiOptions ^= View.SYSTEM_UI_FLAG_FULLSCREEN;
+        }
+
+        // Immersive mode: Backward compatible to KitKat.
+        // Note that this flag doesn't do anything by itself, it only augments the behavior
+        // of HIDE_NAVIGATION and FLAG_FULLSCREEN.  For the purposes of this sample
+        // all three flags are being toggled together.
+        // Note that there are two immersive mode UI flags, one of which is referred to as "sticky".
+        // Sticky immersive mode differs in that it makes the navigation and status bars
+        // semi-transparent, and the UI flag does not get cleared when the user interacts with
+        // the screen.
+        if (Build.VERSION.SDK_INT >= 18) {
+            newUiOptions ^= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        }
+
+        getWindow().getDecorView().setSystemUiVisibility(newUiOptions);
+        //END_INCLUDE (set_ui_flags)
+
     }
 }
